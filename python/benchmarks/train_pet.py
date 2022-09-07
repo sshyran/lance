4051750e6c44978a9aff56647718b072430dd7cc
Lei Xu <lei@eto.ai>
2022-09-07 23:01:26 +0000

    fixes

--- python/benchmarks/train_pet.py
+++ python/benchmarks/train_pet.py
@@ -6,11 +6,12 @@
 
 import io
 import os
+import time
 from typing import Callable, Optional
 
 import click
-import pyarrow.fs
 import pyarrow.compute as pc
+import pyarrow.fs
 import pytorch_lightning as pl
 import torch
 import torchvision
@@ -24,14 +25,51 @@ import lance.pytorch.data
 
 transform = T.Compose([EfficientNet_B0_Weights.DEFAULT.transforms()])
 
-
-def raw_collate_fn(batch):
-    images = []
+@profile
+def collate_fn(batch):
     labels = []
-    for img, label in batch:
-        images.append(img)
-        labels.append(label)
-    return torch.stack(images), torch.tensor(labels)
+    # TODO: Labels should be converted via torch.LanceDataset
+    labels = torch.randint(0, 31, size=(len(batch[1]),))
+    # TODO: Image conversion should in torch.LanceDataset
+    images = []
+    for data in batch[0]:
+        img = Image.open(io.BytesIO(data)).convert("RGB")
+        images.append(transform(img))
+    return torch.stack(images), labels
+
+
+@profile
+def lance_collate_fn(benchmark):
+    def collate_fn(batch):
+        labels = []
+        # TODO: Labels should be converted via torch.LanceDataset
+        labels = torch.randint(0, 31, size=(len(batch[1]),))
+        if benchmark == "io":
+            # Skip cpu processing
+            return batch[0], labels
+        # TODO: Image conversion should in torch.LanceDataset
+        images = []
+        for data in batch[0]:
+            img = Image.open(io.BytesIO(data)).convert("RGB")
+            images.append(transform(img))
+        return torch.stack(images), labels
+
+    return collate_fn
+
+
+def raw_collate_fn(benchmark):
+    def collate_fn(batch):
+        images = []
+        labels = []
+        for img, label in batch:
+            images.append(img)
+            labels.append(label)
+        if benchmark == "io":
+            return images, labels
+        else:
+            torch.stack(images), torch.tensor(labels)
+
+    return collate_fn
 
 
 class RawOxfordPetDataset(torch.utils.data.Dataset):
@@ -74,22 +112,21 @@ class RawOxfordPetDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.images)
 
+    @profile
     def __getitem__(self, idx):
         image_uri = os.path.join(self.root, "images", f"{self.images[idx]}.jpg")
         fs, path = pyarrow.fs.FileSystem.from_uri(image_uri)
-        with fs.open_input_stream(path) as fobj:
-            img = Image.open(io.BytesIO(fobj.readall())).convert('RGB')
-            if self.transform:
-                img = self.transform(img)
-            return img, self.labels[idx]
-
-
-def collate_fn(batch):
-    # TODO: Labels should be converted via torch.LanceDataset
-    labels = torch.randint(0, 31, size=(len(batch[1]),))
-    # TODO: Image conversion should in torch.LanceDataset
-    images = [transform(Image.open(io.BytesIO(data)).convert('RGB')) for data in batch[0]]
-    return torch.stack(images), labels
+        retries = 5
+        while retries > 0:
+            try:
+                with fs.open_input_stream(path) as fobj:
+                    img = Image.open(io.BytesIO(fobj.readall())).convert("RGB")
+                    if self.transform:
+                        img = self.transform(img)
+                    return img, self.labels[idx]
+            except Exception:
+                retries += 1
+                time.sleep(1)
 
 
 class Classification(pl.LightningModule):
@@ -111,13 +148,13 @@ class Classification(pl.LightningModule):
         https://github.com/pytorch/vision/blob/main/references/classification/train.py
         """
         images, labels = batch
-        if self.benchmark == "io":
-            # only test I/O
-            pass
-        else:
+        if self.benchmark == "train":
             output = self.model(images)
             loss = self.criterion(output, labels)
             return loss
+        else:
+            # skip GPU
+            pass
 
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=1e-3)
@@ -141,8 +178,9 @@ class Classification(pl.LightningModule):
     type=click.Choice(["lance", "raw", "parquet"]),
     default="lance",
 )
-@click.option("--benchmark", type=click.Choice(["io", "train"]), default="train")
+@click.option("--benchmark", type=click.Choice(["io", "cpu", "train"]), default="train")
 @click.argument("dataset")
+@profile
 def train(
     dataset: str,
     batch_size: int,
@@ -157,28 +195,35 @@ def train(
             dataset,
             columns=["image", "class"],
             batch_size=batch_size,
-            #filter=(pc.field("split") == "train")
+            # filter=(pc.field("split") == "train")
         )
         train_loader = torch.utils.data.DataLoader(
             dataset,
             num_workers=num_workers,
             batch_size=None,
-            collate_fn=collate_fn,
+            #collate_fn=lance_collate_fn(benchmark=benchmark),
+            collate_fn=collate_fn
         )
     elif data_format == "raw":
-        dataset = RawOxfordPetDataset(dataset, transform=transform)
+        dataset = RawOxfordPetDataset(
+            dataset, transform=transform if benchmark != "io" else None
+        )
         train_loader = torch.utils.data.DataLoader(
             dataset,
             num_workers=num_workers,
             batch_size=batch_size,
-            collate_fn=raw_collate_fn,
+            collate_fn=raw_collate_fn(benchmark),
         )
     else:
         raise ValueError("Unsupported data format")
 
     model = Classification(benchmark=benchmark)
     trainer = pl.Trainer(
-        limit_train_batches=100, max_epochs=epochs, accelerator="gpu", devices=-1
+        limit_train_batches=100,
+        max_epochs=epochs,
+        accelerator="gpu",
+        devices=-1,
+        # profiler="advanced",
     )
     trainer.fit(model=model, train_dataloaders=train_loader)
 
